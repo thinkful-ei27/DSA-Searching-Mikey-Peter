7b5d52946ed3a5b4b5ad7e3a94a45191ad233eb4
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-03-06 15:56:37 -0500

    dst in binary class

---
+++ search-drills.js
@@ -0,0 +1,153 @@
+/**======Dewey decimal system (DDS)
+since the DDS is a sorted list, we can use a logrithmic sorting function to 
+quickly find the book , given the index.
+If provided sub lists for classs, subclass, book titles,
+respectively, we can use sorted list algorithm at each level, to cut down on
+length of total list and find the book quicker
+ */
+
+
+
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    }
+    else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this.key);
+      }
+      else {
+        this.left.insert(key, value);
+      }
+    }
+    else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this.key);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (this.key === key) {
+      return this.value;
+    }
+    else if (this.key < key && this.left) {
+      return this.left.find(key);
+    }
+    else if (this.key > key && this.right) {
+      return this.right.find(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (this.key === key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        this.remove(successor, key);
+      }
+      else if (this.left) {
+        this._replaceWith(this.right);
+      }
+      else if (this.right) {
+        this._replaceWith(this.left);
+      }
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  dfs(values = []) {
+    if (this.left) {
+      values = this.left.dfs(values);
+    }
+    values.push(this.value);
+
+    if (this.right) {
+      values = this.right.dfs(values);
+    }
+    return values;
+  }
+
+  dfsPre(values = []) {
+
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this === this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this === this.parent.right) {
+        this.parent.right = node;
+      }
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.right = node.right;
+        this.left = node.left;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.right = null;
+        this.left = null;
+
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
+}
+
+let dataList = [25, 15, 50, 10, 24, 35, 70, 4, 12, 18, 31, 44, 66, 90, 22]
+
+function createBst(list) {
+  const BST = new BinarySearchTree();
+  for (let i = 0; i < list.length; i++) {
+    BST.insert(list[i], list[i]);
+  }
+  return BST;
+}
+const bstToSort = createBst(dataList);
+console.log(JSON.stringify(bstToSort, null, 2));
+
+console.log(bstToSort.dfs());
